e1c7b6e3199315e03419fcd45cc280fc3ef71009
Regio Abundan <regioricco21@gmail.com>
2018-05-30 19:39:34 +0800

    Additional files

--- Visualizing Volatility/volatility.py
+++ Visualizing Volatility/volatility.py
@@ -3,20 +3,24 @@ from pandas_datareader import data as web
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-start = datetime(2016,1,1)
-end = datetime(2017,1,1)
+start = datetime(2017,1,1)
+end = datetime(2018,1,1)
 
-assets = ['AAPL', 'FB', 'TSLA']
+# FANG stocks
+assets = ['AAPL', 'FB', 'NFLX', 'GOOGL']
 
 df = pd.DataFrame()
 
 for stock in assets:
-    df[stock] = web.DataReader(stock, data_source='yahoo', start=start, end=end)['Adj Close']
+    df[stock] = web.DataReader(stock, data_source='quandl', start=start, end=end)['AdjClose']
 
 asset_returns_daily = df.pct_change()
 asset_volatility_daily = asset_returns_daily.std()
 
-asset_returns_daily.plot(figsize=(10,6))
-asset_volatility_daily.plot(figsize=(10,6))
+asset_returns_daily.plot(figsize = (10,6)) # .hist(bins = 50, figsize = (10,6))
 plt.xlabel('Daily Returns')
+plt.show()
+
+asset_returns_daily.plot.hist(bins = 50, figsize = (10,6))
+plt.xlabel('Daily Returns Histogram')
 plt.show()
